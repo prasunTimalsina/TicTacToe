14244dcf377479b8f8521f6c9d59f2b9a0ba8bd2
prasunTimalsina <timalsinaprasun61@gmail.com>
2024-11-21 18:00:49 +0545

    Add modal window and refactor game logic

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const GameBoard = function () {
 
   const getBoard = () => board;
 
-  const mark = function (row, column, player) {
+  const markCell = function (row, column, player) {
     board[row][column].mark = player.mark;
   };
 
@@ -20,7 +20,7 @@ const GameBoard = function () {
     console.log(board);
   };
 
-  return { getBoard, mark, printboard, board };
+  return { getBoard, markCell, printboard, board };
 };
 
 ///Cell
@@ -56,10 +56,11 @@ const GameController = function () {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
 
+  //Playing a round
   const playRound = function (row, column) {
     console.log(`${activePlayer.name} is paying now...`);
-    Gameboard.mark(row, column, activePlayer);
-    collectCellsData();
+    Gameboard.markCell(row, column, activePlayer);
+    /* collectCellsData(); */
     if (checkWinner()) {
       console.log(`${activePlayer.name} won the game.`);
       return;
@@ -67,49 +68,11 @@ const GameController = function () {
     switchPlayer();
   };
 
-  let rightDiagonalCells = [];
-  let leftDiagonalCells = [];
-  let firstRowCells = [];
-  let secondRowCells = [];
-  let thirdRowCells = [];
-  let firstColCells = [];
-  let secondColCells = [];
-  let thirdColCells = [];
-
-  const collectCellsData = function () {
-    rightDiagonalCells = [];
-    leftDiagonalCells = [];
-    firstRowCells = [];
-    secondRowCells = [];
-    thirdRowCells = [];
-    firstColCells = [];
-    secondColCells = [];
-    thirdColCells = [];
-    for (let row = 0; row < 3; row++) {
-      for (let column = 0; column < 3; column++) {
-        /*   console.log("Collecting cell data..."); */
-        if (row === column) leftDiagonalCells.push(board[row][column]);
-
-        if (row === 0) firstRowCells.push(board[row][column]);
-
-        if (row === 1) secondRowCells.push(board[row][column]);
-
-        if (row === 2) thirdRowCells.push(board[row][column]);
-
-        if (column === 0) firstColCells.push(board[row][column]);
-
-        if (column === 1) secondColCells.push(board[row][column]);
-
-        if (column === 2) {
-          thirdColCells.push(board[row][column]);
-        }
-
-        if (row + column === 2) {
-          rightDiagonalCells.push(board[row][column]);
-        }
-      }
-    }
-  };
+  //const getRow = (row) => board[row]; ::: this is not use cause for the row we can simply use board array
+  const getColumn = (col) => board.map((row) => row[col]);
+  const getLeftDiagonal = () => board.map((row, i) => row[i]);
+  const getRightDiagonal = () =>
+    board.map((row, i) => row[board.length - 1 - i]);
 
   const checkTriplet = function (Cells) {
     if (Cells.every((cell) => cell.mark === "X")) return true;
@@ -117,28 +80,22 @@ const GameController = function () {
     else return false;
   };
 
+  const getWinningCombination = () => [
+    ...board,
+    [0, 1, 2].map(getColumn),
+    getLeftDiagonal(),
+    getRightDiagonal(),
+  ];
+
   const checkWinner = function () {
-    if (
-      checkTriplet(rightDiagonalCells) ||
-      checkTriplet(leftDiagonalCells) ||
-      checkTriplet(firstColCells) ||
-      checkTriplet(secondColCells) ||
-      checkTriplet(thirdColCells) ||
-      checkTriplet(firstRowCells) ||
-      checkTriplet(secondRowCells) ||
-      checkTriplet(thirdRowCells)
-    ) {
-      return true;
-    } else {
-      return false;
-    }
+    return getWinningCombination().some(checkTriplet);
   };
 
   return {
     board,
     players,
     playRound,
-    collectCellsData,
+
     checkWinner,
     checkTriplet,
   };
